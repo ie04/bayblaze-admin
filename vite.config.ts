86848a597c401aa06526f5f5ab06c8b3fd4e80be
ie04 <26415712+ie04@users.noreply.github.com>
2026-07-25 17:59:47 -0400

    Add admin favicon and PWA icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,20 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icons/bayblaze-admin-icon-192.svg",
+            src: "/icons/icon-192.png",
             sizes: "192x192",
-            type: "image/svg+xml",
+            type: "image/png",
           },
           {
-            src: "/icons/bayblaze-admin-icon-512.svg",
+            src: "/icons/icon-512.png",
             sizes: "512x512",
-            type: "image/svg+xml",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           }
         ],
       },
